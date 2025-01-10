0bc306e7970b07a5c4b17995310eb0c9ce0d33e1
masayoshikuroda <kuroda.masayoshi@gmail.com>
2025-01-10 12:19:45 +0900

    Change Yahoo transit URL 	modified:   yahootransit.rb

--- yahootransit.rb
+++ yahootransit.rb
@@ -5,22 +5,24 @@ require 'open-uri'
 class YahooTransit
   attr_reader :title, :icon, :status, :info
 
-  BASE_URL = 'https://transit.yahoo.co.jp/traininfo/detail'
+  BASE_URL = 'https://transit.yahoo.co.jp/diainfo'
 
   def initialize(a, b)
     url = getUrl(a, b)
-    html = open(url) do |f| f.read end
+    html = URI.open(url) do |f| f.read end
     page = Nokogiri::HTML.parse(html, nil, 'UTF-8')
+    
+    p page.css("div#mdServiceStatus > dl > dt").inner_text
 
     @title = page.css("h1.title").inner_text
     text = page.css("div#mdServiceStatus > dl > dt").inner_text
-    @icon = text.split('[')[1].split(']')[0]
-    @status = text.split(']')[1]
+    @icon = page.css("div#mdServiceStatus > dl > dt > span").first.attr('class')
+    @status = page.css("div#mdServiceStatus > dl > dt").inner_text
     @info = page.css("div#mdServiceStatus > dl > dd > p").inner_text
   end
 
   def getUrl(a, b)
-    return "#{BASE_URL}/%d/%d/" % [a, b]
+    return "#{BASE_URL}/%d/%d" % [a, b]
   end
 end
 
